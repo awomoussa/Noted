9c74fcf461aaa301bf215d7f26fa71254c5fe6fc
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2023-05-19 13:11:05 -0600

    creating home page

--- client/src/pages/home/home.jsx
+++ client/src/pages/home/home.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from "react";
+import "./home.scss"
 
 export default function Home() {
   return (
-    <h1>HOME!</h1>
-  )
+    <section className="main-container">
+      <div className="content">
+        <div className="hero-section">
+          <h1>SLOGAN HERE</h1>
+          <div className="hero-content">
+          <div className="hero-image">IMAGE HERE</div>
+          <p className="hero-text">Noted - the perfect note-taking app where every idea, is duly noted.</p>
+          </div>
+        </div>
+
+        <div className="howto-section">
+          <div className="howto-steps">
+            <div className="step1">STEP 1 box</div>
+            <div className="step2">STEP 2 box</div>
+            <div className="step3">STEP 3 box</div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+  );
 }
