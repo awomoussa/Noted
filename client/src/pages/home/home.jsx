da0a842771d95245f20abb55ceed6eefa9011067
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2023-05-25 13:41:15 -0600

    working on home page

--- client/src/pages/home/home.jsx
+++ client/src/pages/home/home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./home.scss"
+import home from "../../assets/home.jpg"
 
 export default function Home() {
   return (
@@ -8,7 +9,9 @@ export default function Home() {
         <div className="hero-section">
           <h1>Simplify Your Notes</h1>
           <div className="hero-content">
-          <div className="hero-image">IMAGE HERE</div>
+          <div className="hero-image">
+            <img src={home}></img>
+          </div>
           <p className="hero-text">Noted - the perfect note-taking app where every idea, is duly noted.</p>
           </div>
         </div>
@@ -20,9 +23,9 @@ export default function Home() {
             <div className="step3">STEP 3 box</div>
           </div>
           <div className="howto-steps">
-            <div className="step1">STEP 1 box</div>
-            <div className="step2">STEP 2 box</div>
-            <div className="step3">STEP 3 box</div>
+            <div className="">STEP 1 instructions</div>
+            <div className="">STEP 2 instructionsox</div>
+            <div className="">STEP 3 instructions</div>
           </div>
         </div>
 
